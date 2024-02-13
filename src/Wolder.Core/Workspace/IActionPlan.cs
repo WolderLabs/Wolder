55d245eafcd18246bc5c1bcd77901ff659d8c752
Zach Starkebaum <zach@relevantwalk.com>
2024-02-13 07:31:54 -0800

    Refactor invokable classes and add new interfaces
    Removed 'Invoke.cs' file and added new interfaces 'IInvokeActionPlan', 'IInvoker', 'IInvokeAction' and 'IActionPlan'. Also, moved the invokable classes functionality into 'InvocationMiddleware' to simplify the invocation process and provide more flexibility.

---
+++ src/Wolder.Core/Workspace/IActionPlan.cs
@@ -0,0 +1,23 @@
+﻿namespace Wolder.Core.Workspace;
+
+public interface IVoidActionPlan<in TParameter> : IVoidInvokable<TParameter>, IInvokableActionPlan
+    where TParameter : notnull
+{
+}
+
+public interface IActionPlan<TOutput> : IInvokable<TOutput>, IInvokableActionPlan
+{
+}
+
+public interface IActionPlan<in TParameter, TOutput> : IInvokable<TParameter, TOutput>, IInvokableActionPlan
+    where TParameter : notnull
+{
+}
+
+public interface IVoidActionPlan : IVoidInvokable, IInvokableActionPlan
+{
+}
+
+public interface IInvokableActionPlan
+{
+}
---
+++ src/Wolder.Core/Workspace/IInvoker.cs
@@ -0,0 +1,18 @@
+﻿namespace Wolder.Core.Workspace;
+
+public interface IInvoker // I don't love this name
+{
+    Task InvokeVoidAsync<TInvokable>()
+        where TInvokable : IVoidInvokable;
+
+    Task InvokeAsync<TInvokable, TParameter>(TParameter parameter)
+        where TInvokable : IVoidInvokable<TParameter>
+        where TParameter : notnull;
+
+    Task<TOutput> InvokeAsync<TInvokable, TParameter, TOutput>(TParameter parameter)
+        where TInvokable : IInvokable<TParameter, TOutput>
+        where TParameter : notnull;
+
+    Task<TOutput> InvokeAsync<TInvokable, TOutput>()
+        where TInvokable : IInvokable<TOutput>;
+}
